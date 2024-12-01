2a6661dab11e406f6a3f1faec7d55c730c9501cc
Guy Luz <guyluz11@gmail.com>
2024-12-01 16:18:11 +0200

    App bar text starts on the left

--- lib/presentation/atoms/atoms.dart
+++ lib/presentation/atoms/atoms.dart
@@ -3,6 +3,7 @@ export 'package:word_link/presentation/atoms/card_atom.dart';
 export 'package:word_link/presentation/atoms/check_box_atom.dart';
 export 'package:word_link/presentation/atoms/list_tile_atom.dart';
 export 'package:word_link/presentation/atoms/margined_expanded_atom.dart';
+export 'package:word_link/presentation/atoms/popup_menu_entry_atom.dart';
 export 'package:word_link/presentation/atoms/separator_atom.dart';
 export 'package:word_link/presentation/atoms/text_atom.dart';
 export 'package:word_link/presentation/atoms/toggle_switch_atom.dart';
